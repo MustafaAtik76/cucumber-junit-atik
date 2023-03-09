cf8229333d57934d0a929400c0736885a151c927
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-09 23:40:05 +0300

    update

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -71,7 +71,7 @@ public class Driver {
                     break;
                 case "remote-chrome":
                     // assign your grid server address
-                    String gridAdress = "54.160.69.233"; // put your own Linux grid IP here
+                    String gridAdress = "52.87.252.7"; // put your own Linux grid IP here
                     try {
                         URL url = new URL("http://"+gridAdress+":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
